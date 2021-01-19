3503cef7a1d036f1b8f310924639346295283f05
medabde <medabde9@gmail.com>
2021-01-19 08:13:10 +0100

    project setup

---
+++ src/main/java/com/med/expsensetackerapi/ExpsenseTackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.med.expsensetackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpsenseTackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpsenseTackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/med/expsensetackerapi/ExpsenseTackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.expsensetackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpsenseTackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
